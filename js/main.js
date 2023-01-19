d4531efa91083e116ad8a19c9f1b6690dcf0be62
Ryan Lowe <somebody480@gmail.com>
2023-01-18 20:10:50 -0500

    Countdown to next interval added

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
-const audio = document.getElementById("audio");
+let audio = document.getElementById("audio");
 let minInterval = document.getElementById("minInterval").value;
 let maxInterval = document.getElementById("maxInterval").value;
 let intervalId;
+let countdown = document.getElementById("countdown");
+let time = document.getElementById("time");
+let nextPlayback;
+let countdownInterval;
 
-function playAudio() {
+const playAudio = () => {
   audio.play();
-  intervalId = setTimeout(playAudio, randomInterval());
-}
+  nextPlayback = new Date();
+  nextInterval = randomInterval();
+  nextPlayback.setSeconds(nextPlayback.getSeconds() + nextInterval / 1000);
+  intervalId = setTimeout(playAudio, nextInterval);
+  countdown.style.display = "block";
+  countdownInterval = setInterval(showTime, 1000);
+};
 
-function stopAudio() {
+const stopAudio = () => {
   clearTimeout(intervalId);
+  clearInterval(countdownInterval);
   audio.pause();
-}
+  countdown.style.display = "none";
+};
 
-function randomInterval() {
-  minInterval = document.getElementById("minInterval").value;
-  maxInterval = document.getElementById("maxInterval").value;
-  return (
-    Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval
-  );
-}
+const randomInterval = () => {
+  minInterval = document.getElementById("minInterval").value * 1000;
+  maxInterval = document.getElementById("maxInterval").value * 1000;
+  let interval =
+    Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval;
+  console.log(interval);
+  return interval;
+};
+
+const showTime = () => {
+  let now = new Date();
+  let s = Math.floor((nextPlayback - now) / 1000);
+  let m = Math.floor(s / 60);
+  let h = Math.floor(m / 60);
+  let d = Math.floor(h / 24);
+  h %= 24;
+  m %= 60;
+  s %= 60;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+  time.innerHTML = (d ? d + "d " : "") + h + ":" + m + ":" + s;
+};
+
+countdown.style.display = "none";
