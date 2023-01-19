1e131857f72c96cb440f615356428d4958825ea8
Ryan Lowe <somebody480@gmail.com>
2023-01-18 19:40:22 -0500

    add simple bootstrap UI

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-var audio = document.getElementById("audio");
-var minInterval = 120 * 60 * 1000; // minimum interval in milliseconds (1 minute)
-var maxInterval = 2 * 60 * 1000; // maximum interval in milliseconds (2 hours) 120 original
-
-console.log(`Min interval: ${minInterval} Max interval: ${maxInterval}`);
+const audio = document.getElementById("audio");
+let minInterval = document.getElementById("minInterval").value;
+let maxInterval = document.getElementById("maxInterval").value;
+let intervalId;
 
 function playAudio() {
-  console.log("Audio played.");
   audio.play();
-  setTimeout(playAudio, randomInterval());
+  intervalId = setTimeout(playAudio, randomInterval());
+}
+
+function stopAudio() {
+  clearTimeout(intervalId);
+  audio.pause();
 }
 
 function randomInterval() {
+  minInterval = document.getElementById("minInterval").value;
+  maxInterval = document.getElementById("maxInterval").value;
   return (
     Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval
   );
 }
-
-setTimeout(playAudio, randomInterval());
